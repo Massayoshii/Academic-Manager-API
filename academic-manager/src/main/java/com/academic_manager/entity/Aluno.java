159749362a934e1d95c3f2393b401360cc2b198f
Massayoshii <Massayoshibando@gmail.com>
2026-08-24 19:24:40 -0300

    feat: create Aluno entity

---
+++ academic-manager/src/main/java/com/academic_manager/entity/Aluno.java
@@ -0,0 +1,35 @@
+package com.academic_manager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_alunos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "email" , unique = true , nullable = false)
+    private String email;
+
+    @Column(name = "cpf" , unique = true , nullable = false)
+    private String cpf;
+
+    @Column(name = "data_nascimento" , nullable = false)
+    private LocalDate dataNascimento;
+}
