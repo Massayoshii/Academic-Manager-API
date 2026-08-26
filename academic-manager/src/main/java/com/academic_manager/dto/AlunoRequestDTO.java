b6456c5255957907f031101c92ad7056e7b7057f
Massayoshii <Massayoshibando@gmail.com>
2026-08-26 09:29:43 -0300

    feat: add request validation

--- academic-manager/src/main/java/com/academic_manager/dto/AlunoRequestDTO.java
+++ academic-manager/src/main/java/com/academic_manager/dto/AlunoRequestDTO.java
@@ -1,18 +1,25 @@
 package com.academic_manager.dto;
 
-import com.academic_manager.entity.Curso;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 public record AlunoRequestDTO(
+        @NotBlank(message = "O nome é obrigatorio")
         String nome,
+
+        @Email(message = "O email deve ser valido")
+        @NotBlank(message = "O email é obrigatorio")
         String email,
+
+        @NotBlank(message = "O CPF é obrigatorio")
         String cpf,
+
+        @NotNull(message = "A data de nascimento é obrigatorio")
+        @Past(message = "A data de nascimento deve estar no passado")
         LocalDate dataNascimento,
+
+        @NotNull(message = "O curso é obrigatorio")
         Long cursoId
 ) {
 }
--- academic-manager/src/main/java/com/academic_manager/dto/CursoRequestDTO.java
+++ academic-manager/src/main/java/com/academic_manager/dto/CursoRequestDTO.java
@@ -1,7 +1,11 @@
 package com.academic_manager.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CursoRequestDTO(
+        @NotBlank(message = "O nome é obrigatorio")
         String nome,
+        @NotBlank(message = "A descricao é obrigatorio")
         String descricao
 ) {
 }
