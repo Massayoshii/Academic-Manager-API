6c060cfbfa6be1671a6197ce793310c8e01db065
Massayoshii <Massayoshibando@gmail.com>
2026-09-01 11:26:53 -0300

    feat: add request validation in Professor

--- academic-manager/src/main/java/com/academic_manager/dto/ProfessorRequestDTO.java
+++ academic-manager/src/main/java/com/academic_manager/dto/ProfessorRequestDTO.java
@@ -1,8 +1,15 @@
 package com.academic_manager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record ProfessorRequestDTO(
+        @NotBlank(message = "O nome é obrigatorio")
         String nome,
+        @Email(message = "O email deve ser valido")
+        @NotBlank(message = "O email é obrigatorio")
         String email,
+        @NotBlank(message = "A especialidade é obrigatorio")
         String especialidade
 ) {
 }
